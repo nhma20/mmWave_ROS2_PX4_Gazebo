d4a7323c35a0c5907b5ef88c60c3aad685381406
nhma20 <76950970+nhma20@users.noreply.github.com>
2021-06-03 21:15:30 +0200

    Create offboard_control.cpp
    subscribe to velocity vector and apply to drone

---
+++ offboard_control.cpp
@@ -0,0 +1,221 @@
+/****************************************************************************
+ *
+ * Copyright 2020 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/**
+ * @brief Offboard control example
+ * @file offboard_control.cpp
+ * @addtogroup examples
+ * @author Mickey Cowden <info@cowden.tech>
+ * @author Nuno Marques <nuno.marques@dronesolutions.io>
+
+ * The TrajectorySetpoint message and the OFFBOARD mode in general are under an ongoing update.
+ * Please refer to PR: https://github.com/PX4/PX4-Autopilot/pull/16739 for more info. 
+ * As per PR: https://github.com/PX4/PX4-Autopilot/pull/17094, the format
+ * of the TrajectorySetpoint message shall change.
+ */
+
+#include <px4_msgs/msg/offboard_control_mode.hpp>
+#include <px4_msgs/msg/trajectory_setpoint.hpp>
+#include <px4_msgs/msg/timesync.hpp>
+#include <px4_msgs/msg/vehicle_command.hpp>
+#include <px4_msgs/msg/vehicle_control_mode.hpp>
+#include <px4_msgs/msg/debug_vect.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <stdint.h>
+#include <thread>
+
+#include <chrono>
+#include <iostream>
+
+using namespace std::chrono;
+using namespace std::chrono_literals;
+using namespace px4_msgs::msg;
+
+class OffboardControl : public rclcpp::Node {
+public:
+	OffboardControl() : Node("offboard_control") {
+#ifdef ROS_DEFAULT_API
+		offboard_control_mode_publisher_ =
+			this->create_publisher<OffboardControlMode>("OffboardControlMode_PubSubTopic", 10);
+		trajectory_setpoint_publisher_ =
+			this->create_publisher<TrajectorySetpoint>("TrajectorySetpoint_PubSubTopic", 10);
+		vehicle_command_publisher_ =
+			this->create_publisher<VehicleCommand>("VehicleCommand_PubSubTopic", 10);
+#else
+		offboard_control_mode_publisher_ =
+			this->create_publisher<OffboardControlMode>("OffboardControlMode_PubSubTopic");
+		trajectory_setpoint_publisher_ =
+		 	this->create_publisher<TrajectorySetpoint>("TrajectorySetpoint_PubSubTopic");
+		vehicle_command_publisher_ =
+			this->create_publisher<VehicleCommand>("VehicleCommand_PubSubTopic");
+#endif
+
+		timesync_sub_ =
+			this->create_subscription<px4_msgs::msg::Timesync>("Timesync_PubSubTopic", 10,
+				[this](const px4_msgs::msg::Timesync::UniquePtr msg) {
+					timestamp_.store(msg->timestamp);
+				});
+
+
+		subscription_ = this->create_subscription<px4_msgs::msg::DebugVect>(
+			"vel_ctrl_vect_topic",	10,
+			std::bind(&OffboardControl::arm_set_vel, this, std::placeholders::_1));
+
+	}
+	
+	void arm();
+	void disarm();
+
+private:
+	rclcpp::TimerBase::SharedPtr timer_;
+	rclcpp::Subscription<px4_msgs::msg::DebugVect>::SharedPtr subscription_;
+
+	rclcpp::Publisher<OffboardControlMode>::SharedPtr offboard_control_mode_publisher_;
+	rclcpp::Publisher<TrajectorySetpoint>::SharedPtr trajectory_setpoint_publisher_;
+	rclcpp::Publisher<VehicleCommand>::SharedPtr vehicle_command_publisher_;
+	rclcpp::Subscription<px4_msgs::msg::Timesync>::SharedPtr timesync_sub_;
+
+	std::atomic<uint64_t> timestamp_;   //!< common synced timestamped
+	bool armed = false;
+	float xvel = NAN, yvel = NAN, zvel = NAN;
+	uint64_t offboard_setpoint_counter_;   //!< counter for the number of setpoints sent
+
+	void arm_set_vel(const px4_msgs::msg::DebugVect::UniquePtr msg);
+	void publish_offboard_control_mode() const;
+	void publish_trajectory_setpoint(float x, float y, float z, float yaw, float yawspeed, float vx, float vy, float vz) const;
+	void publish_vehicle_command(uint16_t command, float param1 = 0.0,
+				     float param2 = 0.0) const;
+};
+
+
+/**
+ * @brief Control drone velocity based on ROS2 advertiser
+ */
+void OffboardControl::arm_set_vel(const px4_msgs::msg::DebugVect::UniquePtr msg) {
+	if(armed == false){
+		this->publish_vehicle_command(VehicleCommand::VEHICLE_CMD_DO_SET_MODE, 1, 6); // Change to Offboard mode
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		arm(); 	// Arm the vehicle
+	}
+	publish_offboard_control_mode();
+	publish_trajectory_setpoint(NAN,NAN,NAN,NAN,NAN,msg->x,msg->y,msg->z);
+	RCLCPP_INFO(this->get_logger(),  "var: vx: %f, vy: %f, vz: %f'", msg->x, msg->y, msg->y);
+}
+
+/**
+ * @brief Send a command to Arm the vehicle
+ */
+void OffboardControl::arm() {
+	armed = true;
+	publish_vehicle_command(VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 1.0);
+	RCLCPP_INFO(this->get_logger(), "Arm command send");
+}
+
+/**
+ * @brief Send a command to Disarm the vehicle
+ */
+void OffboardControl::disarm() {
+	armed = false;
+	publish_vehicle_command(VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 0.0);
+	RCLCPP_INFO(this->get_logger(), "Disarm command send");
+}
+
+/**
+ * @brief Publish the offboard control mode.
+ *        For this example, only position and altitude controls are active.
+ */
+void OffboardControl::publish_offboard_control_mode() const {
+	OffboardControlMode msg{};
+	msg.timestamp = timestamp_.load();
+	msg.position = true;
+	msg.velocity = true;
+	msg.acceleration = false;
+	msg.attitude = false;
+	msg.body_rate = false;
+
+	offboard_control_mode_publisher_->publish(msg);
+}
+
+
+/**
+ * @brief Publish a trajectory setpoint
+ */
+void OffboardControl::publish_trajectory_setpoint(float x, float y, float z, float yaw, float yawspeed, float vx, float vy, float vz) const {
+	TrajectorySetpoint msg{};
+	msg.timestamp = timestamp_.load();
+	msg.x = x; 		// in meters NED
+	msg.y = y; 		// in meters NED
+	msg.z = z; 		// in meters NED
+	msg.yaw = yaw; 	// in radians NED -PI..+PI
+	msg.yawspeed = yawspeed; 	// in radians/sec
+	msg.vx = vx; 		// in meters/sec
+	msg.vy = vy; 		// in meters/sec
+	msg.vz = vz; 	// in meters/sec
+	//msg.acceleration	// in meters/sec^2
+	//msg.jerk		// in meters/sec^3
+	//msg.thrust		// normalized thrust vector in NED
+
+	trajectory_setpoint_publisher_->publish(msg);
+
+}
+
+/**
+ * @brief Publish vehicle commands
+ * @param command   Command code (matches VehicleCommand and MAVLink MAV_CMD codes)
+ * @param param1    Command parameter 1
+ * @param param2    Command parameter 2
+ */
+void OffboardControl::publish_vehicle_command(uint16_t command, float param1,
+					      float param2) const {
+	VehicleCommand msg{};
+	msg.timestamp = timestamp_.load();
+	msg.param1 = param1;
+	msg.param2 = param2;
+	msg.command = command;
+	msg.target_system = 1;
+	msg.target_component = 1;
+	msg.source_system = 1;
+	msg.source_component = 1;
+	msg.from_external = true;
+
+	vehicle_command_publisher_->publish(msg);
+}
+
+int main(int argc, char* argv[]) {
+	std::cout << "Starting offboard control node..." << std::endl;
+	setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+	rclcpp::init(argc, argv);
+	rclcpp::spin(std::make_shared<OffboardControl>());
+
+	rclcpp::shutdown();
+	return 0;
+}
