746ead0cbf5fe86e2fb27fea9797c60f40a9b1fc
nhma20 <76950970+nhma20@users.noreply.github.com>
2021-07-03 11:04:17 +0200

    lidar-to-sparse-pointcloud implementation

--- vel_ctrl_vec_pub.cpp
+++ vel_ctrl_vec_pub.cpp
@@ -3,13 +3,19 @@
 #include <sensor_msgs/msg/laser_scan.hpp>
 #include <sensor_msgs/msg/image.hpp>
 #include <std_msgs/msg/float32.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <sensor_msgs/msg/point_field.hpp>
 
+#include <algorithm>
+#include <cstdlib>
 #include <stdlib.h> 
 #include <iostream>
 #include <chrono>
 #include <ctime>    
 #include <math.h>  
 #include <limits>
+#include <vector>
+#include <string>
 
 
 using namespace std::chrono_literals;
@@ -26,9 +32,15 @@ class VelocityControlVectorAdvertiser : public rclcpp::Node
 			
 			hor_pix_publisher_ = this->create_publisher<std_msgs::msg::Float32>("/horisontal_cable_pixel", 10);
 			
+			lidar_to_mmwave_pcl_publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("/lidar_to_mmwave_pcl", 10);
+
+			lidar_to_mmwave_pcl_subscription_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+			"/lidar_to_mmwave_pcl",	10,
+			std::bind(&VelocityControlVectorAdvertiser::OnDepthMsg, this, std::placeholders::_1));
+
 			subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
 			"/dist_sensor/laser_scan",	10,
-			std::bind(&VelocityControlVectorAdvertiser::OnDepthMsg, this, std::placeholders::_1));
+			std::bind(&VelocityControlVectorAdvertiser::lidar_to_mmwave_pcl, this, std::placeholders::_1));
 			
 			camera_subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
 			"/cable_camera/image_raw",	10,
@@ -39,13 +51,19 @@ class VelocityControlVectorAdvertiser : public rclcpp::Node
 		rclcpp::TimerBase::SharedPtr timer_;
 		rclcpp::Publisher<px4_msgs::msg::DebugVect>::SharedPtr publisher_;
 		rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr hor_pix_publisher_;
+		rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr lidar_to_mmwave_pcl_publisher_;
 		rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscription_;
 		rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr camera_subscription_;
+		rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr lidar_to_mmwave_pcl_subscription_;
+
 		float shortestDistAngle_;
 		float shortestDist_;
+		std::vector<float> objects_dist;
+		std::vector<float> objects_angl;
 		int callback_count = 0;
-		void OnDepthMsg(const sensor_msgs::msg::LaserScan::SharedPtr _msg);
+		void OnDepthMsg(const sensor_msgs::msg::PointCloud2::SharedPtr _msg);
 		void OnCameraMsg(const sensor_msgs::msg::Image::SharedPtr _msg);
+		void lidar_to_mmwave_pcl(const sensor_msgs::msg::LaserScan::SharedPtr _msg);
 		void VelocityDroneControl(float xv, float yv, float zv);
 		bool beginHover();
 };
@@ -64,30 +82,10 @@ void VelocityControlVectorAdvertiser::VelocityDroneControl(float xv, float yv, f
 }
 
 
-// Lidar message callback function
-void VelocityControlVectorAdvertiser::OnDepthMsg(const sensor_msgs::msg::LaserScan::SharedPtr _msg){
-	float angle_increment = _msg->angle_increment;
-	float angle_min = _msg->angle_min;
-	float angle_max = _msg->angle_max;
-	float total_angle = angle_max - angle_min;
-	int num_of_rays = round(total_angle / angle_increment); // 1000
-	
-	// get shortest dist index
-	int shortestDistIdx = 0;
-	for(int i = 0; i < num_of_rays; i++){
-		if(_msg->ranges[shortestDistIdx] > _msg->ranges[i]){
-			shortestDistIdx = i;
-		}
-	}
-	
-	// angle compared to straight up from drone
-	float shortestDistIdxAngle = float(shortestDistIdx)*angle_increment - angle_max; 
-	std::cout << "angle: " << shortestDistIdxAngle << std::endl;
-	RCLCPP_INFO(this->get_logger(),  "Dist: %f, Angle: %f'", _msg->ranges[shortestDistIdx], shortestDistIdxAngle);
+// mmwave message callback function
+void VelocityControlVectorAdvertiser::OnDepthMsg(const sensor_msgs::msg::PointCloud2::SharedPtr _msg){
+	float shortestDistIdxAngle = this->shortestDistAngle_ ;
 
-	this->shortestDist_ = _msg->ranges[shortestDistIdx];
-	this->shortestDistAngle_ = shortestDistIdxAngle;
-	
 	auto vel_ctrl_vect = px4_msgs::msg::DebugVect();
 	vel_ctrl_vect.timestamp = std::chrono::time_point_cast<std::chrono::microseconds>(std::chrono::steady_clock::now()).time_since_epoch().count();
 	std::string name = "test";
@@ -100,10 +98,11 @@ void VelocityControlVectorAdvertiser::OnDepthMsg(const sensor_msgs::msg::LaserSc
 	float p_dist = 0.5; // proportional gain for distance controller
 	float p_angle = 3.0; // proportional gain for angle controller
 
+	float shortest_dist = this->shortestDist_;
 	if(hovering == true){
-		if(_msg->ranges[shortestDistIdx] < 10){
+		if(shortest_dist < 10){
 			// create velocity control vector to steer drone towards cable
-			VelocityDroneControl(0, p_angle*(shortestDistIdxAngle-control_angle), -p_dist*(_msg->ranges[shortestDistIdx]-control_distance));
+			VelocityDroneControl(0, p_angle*(shortestDistIdxAngle-control_angle), -p_dist*(shortest_dist-control_distance));
 		
 		} else {	
 			// control drone in square motion if nothing within lidar fov
@@ -154,9 +153,9 @@ bool VelocityControlVectorAdvertiser::beginHover(){
 	return 0;
 }
 
-void VelocityControlVectorAdvertiser::OnCameraMsg(const sensor_msgs::msg::Image::SharedPtr _msg){
-	//std::cout << "Img received, size: " << _msg->width << "x" << _msg->height << std::endl;
 
+// calculate pixel coordinates for 3d->2d projection
+void VelocityControlVectorAdvertiser::OnCameraMsg(const sensor_msgs::msg::Image::SharedPtr _msg){
 	float img_hfov = 1.3962634016;
 	float h_focal_length = (_msg->width * 0.5) / tan(img_hfov * 0.5 ); // in pixels
 	// publish horisontal pixel where nearest object would be
@@ -167,12 +166,133 @@ void VelocityControlVectorAdvertiser::OnCameraMsg(const sensor_msgs::msg::Image:
 		xy_ratio = x_depth/y_depth;
 	}
 	float x_px = -1 * xy_ratio * h_focal_length; // -1 to mirror (pinhole stuff)
-	//std::cout << "x_px: " << x_px << std::endl;
 	
+	// publish vertical pixel where nearest object would be
+	 x_depth = sin(0) * this->shortestDist_; // 0 angle because lidar only 2d, no vertical fov
+	 y_depth = cos(0) * this->shortestDist_;
+	 xy_ratio = std::numeric_limits<float>::max(); 
+	if( y_depth != 0 ){
+		xy_ratio = x_depth/y_depth;
+	}
+	float y_px = -1 * xy_ratio * h_focal_length; // -1 to mirror (pinhole stuff)
+
 	auto float32_msg = std_msgs::msg::Float32();
 	float32_msg.data = x_px;
 	this->hor_pix_publisher_->publish(float32_msg);
 }
+
+
+// converts lidar data to pointcloud of detected objects to simulate sparse mmwave
+void VelocityControlVectorAdvertiser::lidar_to_mmwave_pcl(const sensor_msgs::msg::LaserScan::SharedPtr _msg){
+	float angle_increment = _msg->angle_increment;
+	float angle_min = _msg->angle_min;
+	float angle_max = _msg->angle_max;
+	float range_max = _msg->range_max;
+	float range_min = _msg->range_min;
+	float total_angle = angle_max - angle_min;
+	int num_of_rays = round(total_angle / angle_increment); // 1000
+	
+	// get shortest dist index
+	int shortestDistIdx = 0;
+	for(int i = 0; i < num_of_rays; i++){
+		if(_msg->ranges[shortestDistIdx] > _msg->ranges[i]){
+			shortestDistIdx = i;
+		}
+	}
+
+	// group objects in lidar fov to simulate mmwave readings
+	std::vector<float> object_center_angls;
+	std::vector<float> object_center_dists;
+	int grouped_previous = 0;
+	float group_dist = 0;
+	float group_angl = 0;
+	for(int i = 0; i < num_of_rays-1; i++){
+		if(_msg->ranges[i] > range_min && _msg->ranges[i] < range_max){
+			//std::cout << "Object detected, range: " << _msg->ranges[i] << std::endl;
+			if(grouped_previous == 0){	
+				//std::cout << "First beam of object" << std::endl;
+				group_dist += _msg->ranges[i];
+				group_angl += float(i)*angle_increment - angle_max;
+			}
+			if( abs(_msg->ranges[i+1] - _msg->ranges[i]) < 0.1 ){
+				//std::cout << "Object more than one beam wide" << std::endl;
+				group_dist += _msg->ranges[i+1];
+				group_angl += float(i+1)*angle_increment - angle_max;
+				grouped_previous++;
+			}
+			else{
+				//std::cout << "End of object detected, pushing" << std::endl;
+				object_center_dists.push_back( group_dist/(grouped_previous+1) );
+				object_center_angls.push_back( group_angl/(grouped_previous+1) );
+				grouped_previous = 0;
+				group_dist = 0;
+				group_angl = 0;
+			}
+		}
+	}
+	this->objects_angl = object_center_angls;
+	this->objects_dist = object_center_dists;
+	
+	// angle compared to straight up from drone
+	float shortestDistIdxAngle = float(shortestDistIdx)*angle_increment - angle_max; 
+	std::cout << "angle: " << shortestDistIdxAngle << std::endl;
+	RCLCPP_INFO(this->get_logger(),  "Dist: %f, Angle: %f'", _msg->ranges[shortestDistIdx], shortestDistIdxAngle);
+
+	this->shortestDist_ = _msg->ranges[shortestDistIdx];
+	this->shortestDistAngle_ = shortestDistIdxAngle;
+
+	// convert from "spherical" to cartesian
+	std::vector<float> pcl_x;
+	std::vector<float> pcl_y;
+	std::vector<float> pcl_z;
+	for(int i = 0; i<objects_dist.size(); i++){
+		pcl_x.push_back( sin(object_center_angls.at(i)) * object_center_dists.at(i) );
+		pcl_y.push_back( sin(0) * object_center_dists.at(i) );
+		pcl_z.push_back( cos(object_center_angls.at(i)) * object_center_dists.at(i) );
+	}
+
+	// create PointCloud2 msg
+	//https://github.com/ros-drivers/velodyne/blob/master/velodyne_laserscan/tests/system.cpp
+	auto pcl2_msg = sensor_msgs::msg::PointCloud2();
+	pcl2_msg.header = std_msgs::msg::Header();
+	pcl2_msg.header.stamp = this->now();
+	std::string frameID = "map";
+	pcl2_msg.header.frame_id = frameID;
+	pcl2_msg.fields.resize(3);
+	pcl2_msg.fields[0].name = 'x';
+	pcl2_msg.fields[0].offset = 0;
+	pcl2_msg.fields[0].datatype = sensor_msgs::msg::PointField::FLOAT32;
+	pcl2_msg.fields[0].count = 1;
+	pcl2_msg.fields[1].name = 'y';
+	pcl2_msg.fields[1].offset = 4;
+	pcl2_msg.fields[1].datatype = sensor_msgs::msg::PointField::FLOAT32;
+	pcl2_msg.fields[1].count = 1;
+	pcl2_msg.fields[2].name = 'z';
+	pcl2_msg.fields[2].offset = 8;
+	pcl2_msg.fields[2].datatype = sensor_msgs::msg::PointField::FLOAT32;
+	pcl2_msg.fields[2].count = 1;
+	const uint32_t POINT_STEP = 12;
+	pcl2_msg.data.resize(std::max((size_t)1, objects_dist.size()) * POINT_STEP, 0x00);
+	pcl2_msg.point_step = POINT_STEP; // size (bytes) of 1 point (float32 * dimensions (3 when xyz))
+	pcl2_msg.row_step = pcl2_msg.data.size();//pcl2_msg.point_step * pcl2_msg.width; // only 1 row because unordered
+	pcl2_msg.height = 1;  // because unordered cloud
+	pcl2_msg.width = pcl2_msg.row_step / POINT_STEP; // number of points in cloud
+	pcl2_msg.is_dense = false; // there may be invalid points
+
+	// fill PointCloud2 msg data
+	uint8_t *ptr = pcl2_msg.data.data();
+	for (size_t i = 0; i < objects_dist.size(); i++)
+	{
+		*(reinterpret_cast<float*>(ptr + 0)) = pcl_x.at(i);
+		*(reinterpret_cast<float*>(ptr + 4)) = pcl_y.at(i);
+		*(reinterpret_cast<float*>(ptr + 8)) = pcl_z.at(i);
+		ptr += POINT_STEP;
+	}
+	// publish PointCloud2 msg
+	this->lidar_to_mmwave_pcl_publisher_->publish(pcl2_msg);
+}
+
+
 	
 			
 int main(int argc, char *argv[])
