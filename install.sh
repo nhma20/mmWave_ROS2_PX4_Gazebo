da6f1c68f6f7f1e29038aafae0c2e76483f680c2
nhma20 <76950970+nhma20@users.noreply.github.com>
2021-07-08 12:30:50 +0200

    delete lidar_test_sub

--- install.sh
+++ install.sh
@@ -27,7 +27,6 @@ cp -f $CWD/hca_full_setup.world $PX4FIRMDIR/Tools/sitl_gazebo/worlds/
 cp -f $CWD/d4e_HCAairport.world $PX4FIRMDIR/Tools/sitl_gazebo/worlds/
 
 echo "installing Code files.."
-cp -f $CWD/lidar_test_sub.cpp $PX4ROSDIR/src/px4_ros_com/src/examples/offboard/
 cp -f $CWD/offboard_control.cpp $PX4ROSDIR/src/px4_ros_com/src/examples/offboard/
 cp -f $CWD/vel_ctrl_vec_pub.cpp $PX4ROSDIR/src/px4_ros_com/src/examples/offboard/
 cp -f $CWD/img_proj_depth.py $PX4ROSDIR/src/px4_ros_com/src/examples/offboard/
